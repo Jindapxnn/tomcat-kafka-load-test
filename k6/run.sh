9a888b2fe67be9f340df23d7888354e5cc319669
Jindapol K. <jindapol.koh@gmail.com>
2026-01-28 16:38:38 +0700

    Edit: move docker compose into microservices directory and edit run.sh(replace --env-file to -e for write env at here, use :ro with load-test.js mount)

--- k6/run.sh
+++ k6/run.sh
@@ -3,7 +3,7 @@
 # Run k6 load tests
 docker run --rm -i \
 --network tomcat-kafka_network \
---env-file .env.dev \
--v "$(pwd)/k6/load-test.js:/load-test.js" \
+-e K6_OUT=json=/results/results.json \
+-v "$(pwd)/k6/load-test.js:/load-test.js:ro" \
 -v "$(pwd)/k6/results:/results" \
 grafana/k6 run /load-test.js
